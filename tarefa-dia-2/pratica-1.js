69822be34cb5da7bac001f39cd247818600c298e
Leilson <leilson_silva@yahoo.com.br>
2022-08-24 01:00:23 -0300

    Arq com Variáveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email = "leilson_silva@yahoo.com.br"
 let profissao = "Mecânico de Manutenção"
 let idade = 35
 let pais = "Alemanha"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(pais)
