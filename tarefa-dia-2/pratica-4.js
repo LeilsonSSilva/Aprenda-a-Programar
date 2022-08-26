e08208e66a3a547d51591b32c5d0eb54e8b36cb3
Leilson <leilson_silva@yahoo.com.br>
2022-08-25 21:48:26 -0300

    Opcional

--- tarefa-dia-2/pratica-4.js
+++ tarefa-dia-2/pratica-4.js
@@ -1,26 +1,16 @@
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true}
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true}
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false}
 
-const numero = 1500450271 
-let i = 2
-let ehPrimo = true
 
-while (i <= n) {
-    if (numero % i == 0) {
-        ehPrimo = false
-        break 
-    }
-    i++
+if (produtoC.internacional == true) {
+    
+    let valorImpostos = produtoC.valor * 1.2
+      console.log("O produto é internacional")
+    console.log(valorImpostos)
+
+} else {
+    let valorImpostos = produtoC.valor * 1.12
+    console.log("O produto é nacional")
+    console.log(valorImpostos)   
 }
-
-if (ehPrimo) 
-    console.log(`Sim, o número ${numero} é primo.`)
-else
-    console.log(`Não, o número ${numero} não é primo.`)
-
-i = 2 
-while (numero % i != 0)
-    i++ 
-
-if (numero == i) 
-    console.log(`Sim, o número ${numero} é primo.`)
-else
-    console.log(`Não, o número ${numero} não é primo.`)
